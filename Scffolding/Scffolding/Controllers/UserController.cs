447c904716e141c01ba506329bcaf83789d70c48
meh-ka <mehk886@gmail.com>
2025-01-04 23:13:51 +0100

    premier Scaffolding

---
+++ Scffolding/Scffolding/Controllers/UserController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Scffolding.Models;
+
+namespace Scffolding.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: UserController
+        public ActionResult Index()
+        {
+            List<UserModel> user = new List<UserModel> {
+                new UserModel{Id=1, Name="yek", Family="Rast"},
+                new UserModel{Id=2, Name="doo", Family="Pech"},
+                new UserModel{Id=3, Name="sei", Family="ching"},
+            };
+                
+            return View(user);
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            UserModel user = new UserModel()
+            {
+                Id = 1,
+                Name = "yek",
+                Family = "Rast"
+
+            };
+            return View(user);
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Scffolding/Scffolding/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Scffolding.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Family { get; set; }
+    }
+}
