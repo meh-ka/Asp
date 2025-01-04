a5208a3c4240b0c899cf9bbc79102b068f6cea1f
meh-ka <mehk886@gmail.com>
2025-01-04 18:16:35 +0100

    Suppression des fichiers verrouillés et ajout au .gitignore

---
+++ CreateViewComponent/CreateViewComponent/ViewComponents/BestSellingViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using CreateViewComponent.Models.ViewComponentModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreateViewComponent.ViewComponents
+{
+    public class BestSellingViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string Title)
+        {
+            List<ProductModel> products = new List<ProductModel>() {
+                new ProductModel{Id=1, Name="Math 1", Description="Description 1", Price=100},
+                new ProductModel{Id=1, Name="Math 2", Description="Description 2", Price=100},
+                new ProductModel{Id=1, Name="Math 3", Description="Description 3", Price=100},
+                new ProductModel{Id=1, Name="Math 4", Description="Description 4", Price=100},
+                };
+                    ViewBag.Title=Title;
+            return View("Index", products);
+        }
+    }
+}
---
+++ CreateViewComponent/CreateViewComponent/ViewComponents/FirstViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace CreateViewComponent.ViewComponents
+{
+    public class FirstViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string Text)
+        {
+            ViewBag.Text = Text;
+
+            return View("Index");
+        }
+    }
+}
