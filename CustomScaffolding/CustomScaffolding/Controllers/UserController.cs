cb5a97a406c4de4d93ecec95f804ae145d558ad4
meh-ka <mehk886@gmail.com>
2025-01-05 17:29:52 +0100

    Personnalisation de Scaffolding

---
+++ CustomScaffolding/CustomScaffolding/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using CustomScaffolding.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomScaffolding.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<UserModel> users = new List<UserModel>() {
+            new UserModel{Id=1,Name="yek",Family="rast"},
+            new UserModel{Id=2,Name="doo",Family="peich"},
+            new UserModel{Id=3,Name="sei",Family="chen"},
+            };
+            return View(users);
+        }
+
+        public IActionResult Index2()
+        {
+            List<UserModel> users = new List<UserModel>() {
+            new UserModel{Id=1,Name="yek",Family="rast"},
+            new UserModel{Id=2,Name="doo",Family="peich"},
+            new UserModel{Id=3,Name="sei",Family="chen"},
+            };
+            return View(users);
+        }
+    }
+}
